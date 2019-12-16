e8995f2e6edaf7d6367ca8326034dc91538a0b6f
Braulio Picon <brauliopicon96@gmail.com>
2019-12-15 20:34:33 -0400

    dependencias acomodadas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routesArduino = require("./src/service/Arduino/routesArduinoData");
 const routesMapa = require("./src/service/Mapa/routesMapaData");
+const routes = requiere("./src/service/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 
 routesArduino.assignRoutes(app);
 routesMapa.assignRoutes(app);
+routes.assignRoutes(app);
+
 app.listen(PORT, () => {
   console.log(`El servidor está inicializado en el puerto ${ PORT }`);
  });
