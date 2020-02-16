5b0ab66b6f8f8d57ac1e13b5136ec629bc64814f
Braulio Picon <brauliopicon96@gmail.com>
2020-02-16 18:02:38 -0400

    Implementando la base de datos

--- src/service/User/userData.js
+++ src/service/User/userData.js
@@ -1,18 +1,40 @@
-let data = 
-        [
-            {'name': 'Copo',
-            'lastname': 'Hevia',
-            'id': 1,
-            'position': 'CEO',
-            'user': 'CopitoElLoquito',
-            'password': 12345678},
-        ];
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+})
 
 module.exports = {
 
     getData : function(req, res){
         res.send(data);
     },
+
+    createUser : function (req, res){
+
+        const { nombre, apellido, cedula, cargo, username, password } = req.body
+
+        pool.query('INSERT INTO usuario (usu_nombre, usu_apellido, usu_cedula, usu_cargo, usu_usuario, usu_contrasena) VALUES ($1, $2, $3, $4, $5, $6)', 
+                    [
+                        nombre, 
+                        apellido, 
+                        cedula, 
+                        cargo, 
+                        username, 
+                        password
+                    ],
+                        (error, results) => {
+            if (error) {
+              throw error
+            }
+            res.status(201).send(`User added with ID: ${result.insertId}`)
+          });
+    },
+
 
     addData : function( req, res) {
         var nombre = req.body.name;
