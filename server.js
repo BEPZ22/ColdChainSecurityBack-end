055bab45f7a67fbb558a83946cd9a3de6552df44
braulio.picon@itssca.net <bEPZ22089696$>
2020-03-07 14:36:45 -0400

    Cors problems

--- server.js
+++ server.js
@@ -13,5 +13,13 @@ routes.assignRoutes(app);
 app.listen(PORT, () => {
   console.log(`El servidor está inicializado en el puerto ${ PORT }`);
  });
+ 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 module.exports = app;
