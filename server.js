5c063a75108c88116cbbf13dba6cac3df85e143d
braulio.picon@itssca.net <bEPZ22089696$>
2020-03-07 21:32:01 -0400

    Generalizando el llamado a base de datos

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const bodyParser = require("body-parser");
 const routes = require("./src/service/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const Pool = require('pg').Pool
-
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-});
 
 app.use(bodyParser.json());
 
@@ -31,7 +22,4 @@ app.use((req, res, next) => {
   next();
 });
 
-module.exports = {
-                    app,
-                    pool
-                  }
+module.exports = app;
--- src/service/User/userData.js
+++ src/service/User/userData.js
@@ -1,14 +1,12 @@
-// const Pool = require('pg').Pool
+const Pool = require('pg').Pool
 
-// const pool = new Pool({
-//   user: process.env.DATABASE_USER,
-//   host: process.env.DATABASE_HOST,
-//   database: process.env.DATABASE_NAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   port: process.env.DATABASE_PORT,
-// });
-
-const db = require ('../../../server')
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
 
 const _createUser = "SELECT create_user( $1 , $2 , $3 , $4 , $5 , $6 );";
 const _getUserByID = "SELECT * FROM usuario WHERE usu_cedula = $1";
@@ -19,9 +17,8 @@ const _deleteUser = "SELECT delete_user( $1 );";
 module.exports = {
 
     getAllUsers : async function(req, res) {
-
       try {
-        const response = await db.pool.query(_getAllUsers);
+        const response = await pool.query(_getAllUsers);
         res.status(200).send(response.rows);
       } catch (error) {
         res.status(404).send(error);
@@ -34,7 +31,7 @@ module.exports = {
       const { cedula } = req.body
 
       try {
-        const response = await db.pool.query(_getUserByID, [cedula]);
+        const response = await pool.query(_getUserByID, [cedula]);
         res.status(200).send(response.rows);
       } catch (error) {
         res.status(404).send(error);
@@ -46,7 +43,7 @@ module.exports = {
 
         const { nombre, apellido, cedula, cargo, username, password } = req.body
         try{
-          const response = await db.pool.query( _createUser , [ nombre, 
+          const response = await pool.query( _createUser , [ nombre, 
                                                        apellido, 
                                                        cedula, 
                                                        cargo, 
@@ -62,7 +59,7 @@ module.exports = {
     updateUser : async function(req, res){
       const { nombre, apellido, cedula, cargo, username, password } = req.body
       try {
-        const response = await db.pool.query(_updateUser, [ nombre,
+        const response = await pool.query(_updateUser, [ nombre,
                                                          apellido,
                                                          cedula,
                                                          cargo,
@@ -79,7 +76,7 @@ module.exports = {
       const { cedula } = req.body
 
       try {
-        const response = await db.pool.query(_deleteUser, [cedula]);
+        const response = await pool.query(_deleteUser, [cedula]);
         res.status(200).send({'message':'Usuario eliminado exitosamente'});
       } catch (error) {
         res.status(404).send(error);
