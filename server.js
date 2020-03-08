c31c1ecded7808f3bccfeb467018f66d8c067d31
braulio.picon@itssca.net <bEPZ22089696$>
2020-03-08 13:42:06 -0400

    Arreglando el CORS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
  
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", false);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   if ('OPTIONS' == req.method) {
@@ -26,6 +26,6 @@ app.use((req, res, next) => {
   }
 });
 
-//routes.assignRoutes(app);
+routes.assignRoutes(app);
 
-module.exports = app;
+//module.exports = app;
--- src/service/routes.js
+++ src/service/routes.js
@@ -1,17 +1,16 @@
 const arduino = require("./Arduino/arduinoData");
 const user = require("./User/userData");
-const app = require("../../server");
 
 module.exports = {
     
-    assignRoutes : function(){
+    assignRoutes : function(app){
         
-        app.app.get('/arduino', arduino.getData);
-        app.app.post('/arduino', arduino.addData);
-        app.app.get('/usuario', user.getAllUsers);
-        app.app.get('/usuarioID', user.getUserByID);
-        app.app.post('/usuario', user.createUser);
-        app.app.put('/usuario', user.updateUser);
-        app.app.delete('/usuario', user.deleteUser);
+        app.get('/arduino', arduino.getData);
+        app.post('/arduino', arduino.addData);
+        app.get('/usuario', user.getAllUsers);
+        app.get('/usuarioID', user.getUserByID);
+        app.post('/usuario', user.createUser);
+        app.put('/usuario', user.updateUser);
+        app.delete('/usuario', user.deleteUser);
     }
 }
