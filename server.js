d4e10d8ed5ac77a72e8162ed0459067ad2a0e9c5
braulio.picon@itssca.net <bEPZ22089696$>
2020-03-08 12:55:41 -0400

    Arreglando el CORS

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.listen(PORT, () => {
  });
  
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  if ('OPTIONS' == req.method) {
+      res.send(200);
+  } else {
+      next();
+  }
 });
 
 module.exports = app;
