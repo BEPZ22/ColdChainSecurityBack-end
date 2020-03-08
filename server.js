33663a2f2c65ae43962fb91804733adefc7e84c7
braulio.picon@itssca.net <bEPZ22089696$>
2020-03-08 18:40:25 -0400

    Cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   if ('OPTIONS' == req.method) {
       res.send(200);
   } else {
