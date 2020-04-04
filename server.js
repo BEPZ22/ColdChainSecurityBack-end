5bf347304765e10c69e5a8bb27dd93312c4843d8
Braulio Picón <brauliopicon96@gmail.com>
2020-04-04 16:01:32 -0400

    Instalacion libreria cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const routes = require("./src/service/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,16 +11,17 @@ app.listen(PORT, () => {
   console.log(`El servidor está inicializado en el puerto ${ PORT }`);
  });
  
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header("Access-Control-Allow-Headers", "Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
-  if ('OPTIONS' == req.method) {
-      res.send(200);
-  } else {
-      next();
-  }
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header("Access-Control-Allow-Headers", "Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+//   if ('OPTIONS' == req.method) {
+//       res.send(200);
+//   } else {
+//       next();
+//   }
+// });
 
 routes.assignRoutes(app);
