e0c49bba95ad456a9bf5a1efeb06374d908f20f3
Braulio Picón <brauliopicon96@gmail.com>
2020-04-11 15:17:59 -0400

    Cambios conn respecto al login

--- src/service/User/userData.js
+++ src/service/User/userData.js
@@ -56,9 +56,7 @@ module.exports = {
                                                             cedula, 
                                                             cargo, 
                                                             username, 
-                                                            hashPassword,
-                                                            moment(new Date()),
-                                                            moment(new Date())]);         
+                                                            hashPassword]);         
           res.status(201).send({'message':'Usuario creado exitosamente'});
         } catch(error){
           if (error.routine === '_bt_check_unique') {
