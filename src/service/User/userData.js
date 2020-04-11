5e372f39c2663894e37b7a80ba5c2d35214b6282
Braulio Picón <brauliopicon96@gmail.com>
2020-04-11 15:01:44 -0400

    Cambios conn respecto al login

--- src/service/User/userData.js
+++ src/service/User/userData.js
@@ -51,15 +51,14 @@ module.exports = {
         const hashPassword = helper.hashPassword(req.body.password);
         const { nombre, apellido, cedula, cargo, username} = req.body
         try{
-          const date = new Date();
           const response = await pool.query( _createUser , [ nombre, 
                                                             apellido, 
                                                             cedula, 
                                                             cargo, 
                                                             username, 
                                                             hashPassword,
-                                                            date.getTime(),
-                                                            date.getTime()]);         
+                                                            moment(new Date().getTime()).format("X"),
+                                                            moment(new Date().getTime()).format("X")]);         
           res.status(201).send({'message':'Usuario creado exitosamente'});
         } catch(error){
           if (error.routine === '_bt_check_unique') {
