f7e4db2e175f91d8d26b79dd584ac3943174f310
Braulio Picón <brauliopicon96@gmail.com>
2020-05-11 18:01:02 -0400

    acomodando los servicios a partir de la correcion de el modelo de datos Comercio

---
+++ src/service/Commerce/commerceData.js
@@ -0,0 +1,139 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
+
+const _getAllCommerce = "SELECT * FROM get_comercio($1 , $2 );";
+const _getCommerceById = "SELECT * FROM get_comercio_by_id( $1 );";
+const _getCommerceByName = "SELECT * FROM get_comercio_by_nombre( $1 );";
+const _getCommerceByRif = "SELECT * FROM get_comercio_by_rif( $1 );";
+const _getCommerceName = "SELECT com_nombre FROM comercio;";
+const _createCommerce = "SELECT create_comercio( $1 , $2 , $3 , $4 , $5 , $6, $7, $8 , $9, $10, $11, $12, $13);";
+const _updateCommerce = "SELECT update_comercio( $1 , $2 , $3 , $4 , $5 , $6, $7, $8 , $9, $10, $11, $12, $13, $14 );";
+// const _deleteCommerce = "SELECT delete_warehouse( $1 );";
+
+module.exports = {
+
+  getAllCommerce : async function(req, res) {
+    const rol = req.params['rol'];
+    const id_comercio = req.params['comercio'];      
+      try {
+        const response = await pool.query(_getAllCommerce,[ rol, id_comercio ]);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+
+    },
+
+    getCommerceById : async function(req, res){
+      const id = req.params['id'];
+      try {
+        const response = await pool.query(_getCommerceById, [id]);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+
+    },
+
+    getCommerceByName : async function(req, res){
+      const name = req.params['name'];
+      try {
+        const response = await pool.query(_getCommerceByName, [name]);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+
+    },
+
+    getCommerceByRif : async function(req, res){
+      const rif = req.params['rif'];
+      try {
+        const response = await pool.query(_getCommerceByRif, [rif]);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+
+    },
+
+    getCommerceNames : async function(req, res){
+      try {
+        const response = await pool.query(_getCommerceName);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+
+    },
+
+    createCommerce : async function (req, res){
+
+        const { descripcion, nombre, rif, avenida, calle, zona, edificio, apartamento, nro_apartameto, casa, nro_casa, lugar, horario } = req.body
+        try{
+          const response = await pool.query( _createCommerce , [  descripcion,
+                                                                  nombre, 
+                                                                  rif,
+                                                                  avenida, 
+                                                                  calle, 
+                                                                  zona, 
+                                                                  edificio, 
+                                                                  apartamento, 
+                                                                  nro_apartameto, 
+                                                                  casa, 
+                                                                  nro_casa, 
+                                                                  lugar,
+                                                                  horario]);                   
+          res.status(200).send({'message':'Comercio creado exitosamente'});
+        } catch(error){
+          res.status(404).send(error);
+        }
+
+    },
+
+    updateCommerce: async function(req, res){
+      
+      const { nombre, descripcion, rif, avenida, calle, zona, edificio, apartamento, nro_apartameto, casa, nro_casa, lugar, nombreCambiar, horario} = req.body
+      try {
+        const response = await pool.query( _updateCommerce, [ nombre,
+                                                          descripcion,
+                                                          rif,
+                                                          avenida, 
+                                                          calle, 
+                                                          zona, 
+                                                          edificio, 
+                                                          apartamento, 
+                                                          nro_apartameto, 
+                                                          casa, 
+                                                          nro_casa, 
+                                                          lugar,
+                                                          nombreCambiar,
+                                                          horario]);
+        res.status(200).send(response.rows);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+    },
+
+    // deleteCommerce : async function(req, res){
+
+    //   const { placa } = req.body
+    //   try {
+    //     const response = await pool.query(_deleteTruck, [placa]);
+    //     res.status(200).send({'message':'Comercio eliminada exitosamente'});
+    //   } catch (error) {
+    //     res.status(404).send(error);
+    //   }
+
+    // }
+
+
+}
--- src/service/routes.js
+++ src/service/routes.js
@@ -4,6 +4,7 @@ const truck = require("./Truck/truckData");
 const warehouse = require("./Warehouse/warehouseData");
 const place = require("./Place/placeData");
 const login = require("./Login/Controller/login");
+const commerce = require("./Commerce/commerceData");
 const otro = require("./Otros/otros");
 const auth = require("../service/Login/Middleware/authentification")
 
@@ -11,36 +12,53 @@ module.exports = {
     
     assignRoutes : function(app){
         
+        //Arduino
         app.get('/arduino', arduino.getData);
         app.get('/arduino/addData/:Id/:Lg/:Lt/:Tp/:Dt/:Un/:Wh/:Co/:Ua', arduino.addData);
         app.get('/arduino/addData2', arduino.addData2);
         
+        //Comercio
+        app.get('/comercio/:rol/:comercio', auth.verifyToken, commerce.getAllCommerce);
+        app.get('/comercio/id/:id', auth.verifyToken, commerce.getCommerceById);
+        app.get('/comercio/name/:name', auth.verifyToken, commerce.getCommerceByName);
+        app.get('/comercio/rif/:rif', auth.verifyToken, commerce.getCommerceByRif);
+        app.get('/comercios', auth.verifyToken, commerce.getCommerceNames);
+        app.post('/comercio', auth.verifyToken, commerce.createCommerce);
+        app.put('/comercio', auth.verifyToken, commerce.updateCommerce);
+        // app.delete('/comercio', auth.verifyToken, );
+
+        //Usuario
         app.get('/usuario/:rol/:comercio', auth.verifyToken, user.getAllUsers);
         app.get('/usuario/cedula/:id/:rol', auth.verifyToken, user.getUserByID);
         app.post('/usuario', auth.verifyToken, user.createUser);
         app.put('/usuario', auth.verifyToken, user.updateUser);
         app.delete('/usuario', auth.verifyToken, user.deleteUser);
 
+        //Unidad / Transporte Eslabon
         app.get('/unidad/:rol/:comercio', auth.verifyToken, truck.getAllTrucks);
         app.get('/unidad/:placa', auth.verifyToken, truck.getTruckByPlate);
         app.post('/unidad', auth.verifyToken, truck.createTruck);
         app.put('/unidad', auth.verifyToken, truck.updateTruck);
         app.delete('/unidad', auth.verifyToken, truck.deleteTruck);
 
+        //Almacen
         app.get('/almacen/:rol/:comercio', auth.verifyToken, warehouse.getAllWarehouse);
         app.get('/almacen/:nombre', auth.verifyToken, warehouse.getWarehouseByWarehouseName);
         app.get('/almacenes', auth.verifyToken, warehouse.getWarehouseName);
         app.post('/almacen', auth.verifyToken, warehouse.createWarehouse);
-        app.put('/almacen', auth.verifyToken, warehouse.updateWarehouse); //funcionamiento dudoso
+        app.put('/almacen', auth.verifyToken, warehouse.updateWarehouse); 
         app.delete('/almacen', auth.verifyToken, warehouse.deleteWarehouse);
         
+        //Lugar
         app.get('/pais', auth.verifyToken, place.getAllCountries);
         app.get('/estado/:pais', auth.verifyToken, place.getAllStatesByCountries);
 
+        //Roles y Horarios
         app.get('/rol/:rol', auth.verifyToken, otro.getAllRol);
         app.get('/horarioE', auth.verifyToken, otro.getAllEmployeeSchedule);
         app.get('/horarioC', auth.verifyToken, otro.getAllCommerceSchedule);
 
+        //Login
         app.post('/login', login.login);
     }
 }
