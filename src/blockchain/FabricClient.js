fd4638f7c23fa37c78d09194e90d6002e22d8f0e
Braulio Picón <brauliopicon96@gmail.com>
2020-06-13 17:32:52 -0400

    HLF Middleware

--- src/blockchain/FabricClient.js
+++ src/blockchain/FabricClient.js
@@ -17,23 +17,25 @@ class FBClient extends FabricClient {
         var channel = this.getChannel();
         var peer = this.getPeersForOrg()[0];
         tx_id = txid;
-        console.log(channel + "jeje\n" + peer)
+        console.log("\nlogCCSchannel: " + channel + "\n" + "logCCSpeer: " + peer + "\n")
         return channel.sendTransactionProposal(requestData).then(function (results) {
-            console.log("jeje " + results);
+            console.log("\nlogCCSresults: " + results);
             var proposalResponses = results[0];
-	        var proposal = results[1];
+            console.log("\nproposalResponses logCCSresults0: " + results[0]);
+            var proposal = results[1];
+            console.log("\nproposal logCCSresults1: " + results[1]);
 	        let isProposalGood = false;
 	        if (proposalResponses && proposalResponses[0].response &&
 		        proposalResponses[0].response.status === 200) {
 			    isProposalGood = true;
-			    console.log('Transaction proposal was good');
+			    console.log('\nTransaction proposal was good');
 		    } else {
-			    console.error('Transaction proposal was bad');
+			    console.error('\nTransaction proposal was bad');
             }
 
             if (isProposalGood) {
                 console.log(util.format(
-                    'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+                    '\nSuccessfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
                     proposalResponses[0].response.status, proposalResponses[0].response.message));
 
                 var request = {
@@ -61,14 +63,14 @@ class FBClient extends FabricClient {
 
                         var return_status = {event_status : code, tx_id : transaction_id_string};
                         if (code !== 'VALID') {
-                            console.error('The transaction was invalid, code = ' + code);
+                            console.error('\nThe transaction was invalid, code = ' + code);
                             resolve(return_status);
                         } else {
-                            console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
+                            console.log('\nThe transaction has been committed on peer ' + event_hub.getPeerAddr());
                             resolve(return_status);
                         }
                     }, (err) => {
-                        reject(new Error('There was a problem with the eventhub ::'+err));
+                        reject(new Error('\nThere was a problem with the eventhub ::'+err));
                     },
                         {disconnect: true}
                     );
@@ -79,22 +81,22 @@ class FBClient extends FabricClient {
         
                 return Promise.all(promises);
             } else {
-                console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-                throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+                console.error('\nFailed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+                throw new Error('\nFailed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
             }
 
         }).then((results) => {
-            console.log('Send transaction promise and event listener promise have completed');
+            console.log('\nSend transaction promise and event listener promise have completed');
             if (results && results[0] && results[0].status === 'SUCCESS') {
                 console.log('Successfully sent transaction to the orderer.');
             } else {
-                console.error('Failed to order the transaction. Error code: ' + response.status);
+                console.error('\nFailed to order the transaction. Error code: ' + response.status);
             }
 
             if (results && results[1] && results[1].event_status === 'VALID') {
-                console.log('Successfully committed the change to the ledger by the peer');
+                console.log('\nSuccessfully committed the change to the ledger by the peer');
             } else {
-                console.log('Transaction failed to be committed to the ledger due to ::' + results[1].event_status);
+                console.log('\nTransaction failed to be committed to the ledger due to ::' + results[1].event_status);
             }
         }).then(function () {
             return {"trans_id":tx_id["_transaction_id"]};
@@ -106,6 +108,7 @@ class FBClient extends FabricClient {
 
     query(requestData) {
         var channel = this.getChannel();
+        console.log("LogCCSchannel: " + channel);
         return channel.queryByChaincode(requestData).then((response_payloads) => {
             var resultData = JSON.parse(response_payloads.toString('utf8'));
             return resultData;
@@ -121,8 +124,8 @@ class FBClient extends FabricClient {
             }        
             return resultData;
         }).catch((error) => {
-            console.log('handle error here: ', error.message)
-            return ('handle error here: ', error.message)
+            console.log('\nhandle error here: ', error.message)
+            return ('\nhandle error here: ', error.message)
         });
     }
 }
