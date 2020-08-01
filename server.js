b7783f892d2942e5eacbeac0a92fdf517f510350
BraulioPicon <brauliopicon96@gmail.com>
2020-08-01 15:02:23 -0700

    stringify

--- server.js
+++ server.js
@@ -165,8 +165,11 @@ app.get('/hlf/:empresa', async function (req, res) {
         // Evaluate the specified transaction.
         const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidad":"' + unidad.toString() + '"}}');
         var data = JSON.parse(result.toString())
-        var info = JSON.stringify(data)
-        console.log(`Transaction has been evaluated, result data is: ${info[0].Record}`);
+        
+        data.array.forEach(element => {
+            console.log( JSON.stringify(element.Record.temperatura))
+        });
+        // console.log(`Transaction has been evaluated, result data is: ${info[0].Record}`);
         res.status(200).json({data});
   
     } catch (error) {
