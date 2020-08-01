0b2c7834f0b042ef5ce372ec33e9add2c376e3e5
BraulioPicon <brauliopicon96@gmail.com>
2020-08-01 16:00:53 -0700

    stringify

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.use(cors());
 
 routes.assignRoutes(app);
 
+// Obtener toda la informacion de la Red 
+
 app.get('/hlf', async function (req, res) {
   try {
       // Create a new gateway for connecting to our peer node.
@@ -72,6 +74,8 @@ app.get('/hlf', async function (req, res) {
       process.exit(1);
   }
 });
+
+// Obtener la informacion de la Red filtrada por empresa
 
 app.get('/hlf/:empresa', async function (req, res) {
     const empresa = req.params['empresa'];
@@ -107,6 +111,8 @@ app.get('/hlf/:empresa', async function (req, res) {
     }
   });
 
+// Obtener informacion especifica acerca de un elemento que persiste en la red de la Red segun el ID que se maneje
+
   app.get('/hlfHistory/:id', async function (req, res) {
     const id = req.params['id'];
     try {
@@ -141,7 +147,46 @@ app.get('/hlf/:empresa', async function (req, res) {
     }
   });
 
+// Obtener informacion de la red filtrada por las unidades de transporte 
+
   app.get('/hlfUnidad/:unidad', async function (req, res) {
+    const unidad = req.params['unidad'];
+    try {
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+  
+        let connectionProfile = path.join(__dirname, CONNECTION_PROFILE_PATH)
+        const wallet = new FileSystemWallet(FILESYSTEM_WALLET_PATH);
+  
+        await gateway.connect(connectionProfile, {
+            identity: USER_ID,
+            wallet: wallet,
+            discovery: { enabled: false, asLocalhost: true }
+        });
+  
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork(NETWORK_NAME);
+   
+        // Get the contract from the network.
+        const contract = network.getContract(CONTRACT_ID);
+  
+        // Evaluate the specified transaction.
+        const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidad":"' + unidad.toString() + '"}}');
+        var data = JSON.parse(result.toString())
+
+        // console.log(`Transaction has been evaluated, result data is: ${ max }`);
+        res.status(200).json({data});
+  
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
+  });
+
+// Obtener la maxima temperatura registrada en una unidad de transporte
+
+  app.get('/hlfUnidad/Max/:unidad', async function (req, res) {
     const unidad = req.params['unidad'];
     try {
         // Create a new gateway for connecting to our peer node.
@@ -174,7 +219,7 @@ app.get('/hlf/:empresa', async function (req, res) {
  
         });
         console.log(`Transaction has been evaluated, result data is: ${ max }`);
-        res.status(200).json({data});
+        res.status(200).json({tempMax : max});
   
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
@@ -182,6 +227,53 @@ app.get('/hlf/:empresa', async function (req, res) {
         process.exit(1);
     }
   });
+
+// Obtener la minima temperatura registrada en una unidad de transporte
+
+app.get('/hlfUnidad/Min/:unidad', async function (req, res) {
+    const unidad = req.params['unidad'];
+    try {
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+  
+        let connectionProfile = path.join(__dirname, CONNECTION_PROFILE_PATH)
+        const wallet = new FileSystemWallet(FILESYSTEM_WALLET_PATH);
+  
+        await gateway.connect(connectionProfile, {
+            identity: USER_ID,
+            wallet: wallet,
+            discovery: { enabled: false, asLocalhost: true }
+        });
+  
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork(NETWORK_NAME);
+   
+        // Get the contract from the network.
+        const contract = network.getContract(CONTRACT_ID);
+  
+        // Evaluate the specified transaction.
+        const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidad":"' + unidad.toString() + '"}}');
+        var data = JSON.parse(result.toString())
+        var min = Number('0');
+        data.forEach(element => {
+            var max = Number(element.Record.temperatura.toString())
+            if (min > max){
+                min = max
+            }
+ 
+        });
+        console.log(`Transaction has been evaluated, result data is: ${ max }`);
+        res.status(200).json({tempMin : min});
+  
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
+  });
+
+
+// Obtener la informacion de la red filtrada por unidades estaticas
 
   app.get('/hlfUnidadAlmacen/:unidad', async function (req, res) {
     const unidad = req.params['unidad'];
@@ -216,8 +308,97 @@ app.get('/hlf/:empresa', async function (req, res) {
         res.status(500).json({error: error});
         process.exit(1);
     }
+  });
+
+// Obtener la Maxima temperatura de una unidad estatica en la red
+  app.get('/hlfUnidadAlmacen/Max/:unidad', async function (req, res) {
+    const unidad = req.params['unidad'];
+    try {
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+  
+        let connectionProfile = path.join(__dirname, CONNECTION_PROFILE_PATH)
+        const wallet = new FileSystemWallet(FILESYSTEM_WALLET_PATH);
+  
+        await gateway.connect(connectionProfile, {
+            identity: USER_ID,
+            wallet: wallet,
+            discovery: { enabled: false, asLocalhost: true }
+        });
+  
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork(NETWORK_NAME);
+  
+        // Get the contract from the network.
+        const contract = network.getContract(CONTRACT_ID);
+  
+        // Evaluate the specified transaction.
+        const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidadAlmacen":"' + unidad.toString() + '"}}');
+
+        var data = JSON.parse(result.toString())
+        var max = Number('0');
+        data.forEach(element => {
+            var min = Number(element.Record.temperatura.toString())
+            if (min > max){
+                max = min
+            }
+ 
+        });
+        // console.log(`Transaction has been evaluated, result data is: ${data}`);
+        res.status(200).json({tempMax : max});
+  
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
   }); 
 
+// Obtener la minima temperatura de una unidad estatica en la red
+app.get('/hlfUnidadAlmacen/Min/:unidad', async function (req, res) {
+    const unidad = req.params['unidad'];
+    try {
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+  
+        let connectionProfile = path.join(__dirname, CONNECTION_PROFILE_PATH)
+        const wallet = new FileSystemWallet(FILESYSTEM_WALLET_PATH);
+  
+        await gateway.connect(connectionProfile, {
+            identity: USER_ID,
+            wallet: wallet,
+            discovery: { enabled: false, asLocalhost: true }
+        });
+  
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork(NETWORK_NAME);
+  
+        // Get the contract from the network.
+        const contract = network.getContract(CONTRACT_ID);
+  
+        // Evaluate the specified transaction.
+        const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidadAlmacen":"' + unidad.toString() + '"}}');
+
+        var data = JSON.parse(result.toString())
+        var min = Number('0');
+        data.forEach(element => {
+            var max = Number(element.Record.temperatura.toString())
+            if (min > max){
+                min = max
+            }
+ 
+        });
+        // console.log(`Transaction has been evaluated, result data is: ${data}`);
+        res.status(200).json({tempMin : min});
+  
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
+  });
+
+// Introducir infromacion en la red
 app.post('/hlf', async function (req, res) {
   try {
       // Create a new gateway for connecting to our peer node.
