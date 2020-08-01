e821d8d441c845cb7cf5e03a1541651bda5b0169
Veronica Hevia <heviaveronica@gmail.com>
2020-07-31 22:12:32 -0400

    get by WH Unit3

--- server.js
+++ server.js
@@ -194,8 +194,8 @@ app.get('/hlf/:empresa', async function (req, res) {
   
         // Evaluate the specified transaction.
         const result = await contract.evaluateTransaction('queryArduinoData', '{"selector":{"docType":"arduino","unidadAlmacen":"' + unidad.toString() + '"}}');
-        console.log(`Transaction has been evaluated, result is: ${result[0]["temperatura"]}`);
-        res.status(200).send({response: result});
+        console.log(`Transaction has been evaluated, result is: ${result[0]}`);
+        res.status(200).send({response: result.toString()});
   
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
