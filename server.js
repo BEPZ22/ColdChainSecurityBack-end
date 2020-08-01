89b7c02eec37d556d8e243c10b8480ec859bb5ae
Veronica Hevia <heviaveronica@gmail.com>
2020-08-01 00:11:12 -0400

    get by WH Unit result

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.get('/hlf/:empresa', async function (req, res) {
         // result.forEach(value => {
         //     console.log(value.key)
         //   })
-        console.log(`Transaction has been evaluated, result without [] is: ${result.Key}`);
+        console.log(`Transaction has been evaluated, result without [] is: ${result}`);
         res.status(200).send({response: result.toString()});
   
     } catch (error) {
