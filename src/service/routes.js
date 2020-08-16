9f6fa9feadae00de5bf35140db0d92fc88509dda
BraulioPicon <brauliopicon96@gmail.com>
2020-08-16 11:07:43 -0700

    Eslabones por Almacen

--- src/service/routes.js
+++ src/service/routes.js
@@ -58,7 +58,7 @@ module.exports = {
         //Unidad Fija / Eslabon Estatico
         app.get('/eslabonfijo/:rol/:comercio', auth.verifyToken, eslabon.getAllEslabon);
         app.get('/eslabonfijo/:idserial', auth.verifyToken, eslabon.getEslabonByIdSerial);
-        app.get('eslabonfijo/almacen/:warehouseName', auth.verifyToken, eslabon.getEslabonByWarehouse);
+        app.get('eslabonfijoalmacen/:warehouseName', auth.verifyToken, eslabon.getEslabonByWarehouse);
         app.post('/eslabonfijo', auth.verifyToken, eslabon.createEslabon);
         app.put('/eslabonfijo', auth.verifyToken, eslabon.updateEslabon); 
         app.delete('/eslabonfijo', auth.verifyToken, eslabon.deleteEslabon);
